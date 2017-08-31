89cb8e059a998431852e883c02437468828594a1
Joseph Klemen <jklemen@enova.com>
2017-08-31 12:28:48 -0500

    Revised jklemen solution.

--- problem-2017-08/solutions/joe_klemen/point.rb
+++ problem-2017-08/solutions/joe_klemen/point.rb
@@ -12,26 +12,6 @@ module WordSearch
       self.y = y
     end
 
-    def +(point)
-      self.class[x + point.x, y + point.y]
-    end
-
-    def ==(point)
-      x == point.x && y == point.y
-    end
-
-    def hash
-      [x, y].hash
-    end
-
-    def eql?(p)
-      self.==(p)
-    end
-
-    def equal?(p)
-      self.==(p)
-    end
-
     def inspect
       "(#{x}, #{y})"
     end
