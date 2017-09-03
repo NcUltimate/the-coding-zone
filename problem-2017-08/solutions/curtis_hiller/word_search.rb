74bb9606c2a293ac10fa99b5c09ae6f6345b576a
Curtis Hiller <chiller@enova.com>
2017-09-03 10:35:36 -0500

    Refactors Word Search for Execution Speed

--- problem-2017-08/solutions/curtis_hiller/word_search.rb
+++ problem-2017-08/solutions/curtis_hiller/word_search.rb
@@ -1,9 +1,20 @@
+class Descartes
+  attr_accessor :x, :y
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def coordinates
+    [x, y]
+  end
+end
+
 class WordSearch
   attr_accessor :size, :letters, :rows, :words, :highlights
   def initialize(file_path = '')
     @letters = {}
     @words = []
-    @highlights = []
 
     ingest_file!(file_path)
     solve
@@ -19,112 +30,123 @@ class WordSearch
       raise 'Could not open file.'
     end
 
-    temp =  f.readline.split(' ').map(&:to_i)
-    @size = [temp[1], temp[0]]
+    size_array =  f.readline.split(' ').map(&:to_i)
+    @size = [size_array[1], size_array[0]]
     @rows = Array.new(size[0])
+    @highlights = Array.new(size[0])
 
     y = 0
     f.each_line do |line|
+      line = line.gsub(/[^A-Z]/, '')
       line.split('').each_with_index do |letter, x|
         rows[x] ||= []
         rows[x][y] = letter
+        highlights[x] ||= []
+        highlights[x][y] = letter
         letters[letter] ||= []
-        letters[letter] << [x, y]
+        letters[letter] << Descartes.new(x, y)
       end
       y += 1
       break if y == size[1]
     end
 
-    f.each_line { |line| words << line.to_s.gsub(/\s/, '') }
+    f.each_line { |line| words << line.to_s.gsub(/[^A-Z]/, '') }
   end
 
-  def location_has_letter?(x, y, letter)
-    return unless x >= 0 && y >= 0
-    return unless x < size[0] && y < size[1]
-    return true if letter == rows[x][y]
+  def location_has_letter?(coordinates, letter)
+    return unless coordinates.x >= 0 && coordinates.y >= 0
+    return unless coordinates.x < size[0] && coordinates.y < size[1]
+    return true if letter == rows[coordinates.x][coordinates.y]
   end
 
-  def follow_word_path(xy, delta_x, delta_y, word_letters)
-    x = xy[0]
-    y = xy[1]
-    path = []
-    path << [x, y]
+  def follow_word_path(coordinates, delta_x, delta_y, word_letters)
+    path = [coordinates]
 
-    x += delta_x
-    y += delta_y
+    next_coordinates =
+      Descartes.new(coordinates.x + delta_x, coordinates.y + delta_y)
+
     idx = 1
-    while location_has_letter?(x, y, word_letters[idx])
-      path << [x, y]
+    while location_has_letter?(next_coordinates, word_letters[idx])
+      path << next_coordinates
       return path if path.length == word_letters.length
-      x += delta_x
-      y += delta_y
+      next_coordinates = Descartes.new(
+        next_coordinates.x + delta_x,
+        next_coordinates.y + delta_y
+      )
       idx += 1
     end
 
     false
   end
 
-  def find_word_path(xy, word_letters)
+  def find_word_path(coordinates, word_letters)
     word_length = word_letters.length
 
     # left and...
-    if xy[0] >= word_length
+    if coordinates.x >= word_length
       # only left (-1, 0)
-      new_path = follow_word_path(xy, -1, 0, word_letters)
+      new_path = follow_word_path(coordinates, -1, 0, word_letters)
       return new_path if new_path
 
       # down (-1, 1)
-      if xy[1] <= size[1] - word_length
-        new_path = follow_word_path(xy, -1, 1, word_letters)
+      if coordinates.y <= size[1] - word_length
+        new_path = follow_word_path(coordinates, -1, 1, word_letters)
         return new_path if new_path
       end
 
       # up (-1, -1)
-      if xy[1] >= word_length - 1
-        new_path = follow_word_path(xy, -1, -1, word_letters)
+      if coordinates.y >= word_length - 1
+        new_path = follow_word_path(coordinates, -1, -1, word_letters)
         return new_path if new_path
       end
     end
 
     # right and...
-    if xy[0] <= size[0] - word_length
+    if coordinates.x <= size[0] - word_length
       # only right
-      new_path = follow_word_path(xy, 1, 0, word_letters)
+      new_path = follow_word_path(coordinates, 1, 0, word_letters)
       return new_path if new_path
 
       # down (1, 1)
-      if xy[1] <= size[1] - word_length
-        new_path = follow_word_path(xy, 1, 1, word_letters)
+      if coordinates.y <= size[1] - word_length
+        new_path = follow_word_path(coordinates, 1, 1, word_letters)
         return new_path if new_path
       end
 
       # up (1, -1)
-      if xy[1] >= word_length - 1
-        new_path = follow_word_path(xy, 1, -1, word_letters)
+      if coordinates.y >= word_length - 1
+        new_path = follow_word_path(coordinates, 1, -1, word_letters)
         return new_path if new_path
       end
     end
 
     # down (0, 1)
-    if xy[1] <= size[1] - word_length
-      new_path = follow_word_path(xy, 0, 1, word_letters)
+    if coordinates.y <= size[1] - word_length
+      new_path = follow_word_path(coordinates, 0, 1, word_letters)
       return new_path if new_path
     end
 
     # up (0, -1)
-    if xy[1] >= word_length - 1
-      new_path = follow_word_path(xy, 0, -1, word_letters)
+    if coordinates.y >= word_length - 1
+      new_path = follow_word_path(coordinates, 0, -1, word_letters)
       return new_path if new_path
     end
 
     []
   end
 
+  def highlight_letters(letters)
+    letters.each do |coordinates|
+      highlights[coordinates.x][coordinates.y] =
+        "\e[30m\e[46m#{rows[coordinates.x][coordinates.y]}\e[0m\e[0m"
+    end
+  end
+
   def solve
     words.each do |word|
       word_letters = word.split('')
-      letters[word_letters[0]].each do |xy|
-        @highlights += find_word_path(xy, word_letters)
+      letters[word_letters[0]].each do |coordinates|
+        highlight_letters(find_word_path(coordinates, word_letters))
       end
     end
   end
@@ -135,12 +157,7 @@ class WordSearch
       x = 0
       output = ''
       while x < size[0]
-        output <<
-          if highlights.include?([x, y])
-            "\e[30m\e[46m#{rows[x][y]}\e[0m\e[0m"
-          else
-            rows[x][y]
-          end
+        output << highlights[x][y]
         x += 1
       end
       puts output
